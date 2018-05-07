efb2b65421fc0707cc3c1aebdca8c43c6433760e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-05-07 22:50:01 +0200

    should be ok with qpm

--- src/creativecontrolsplugin.hpp
+++ src/creativecontrolsplugin.hpp
@@ -9,8 +9,6 @@ class Plugin final : public QQmlExtensionPlugin
   Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
 public:
   Plugin();
-
-private:
   void registerTypes(const char *uri) final override;
 };
 }
